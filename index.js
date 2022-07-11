01ff5d96bcc5b6b89a088eb5f7d9f35151551a5d
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-07-11 13:47:50 +0100

    added eventListener to ratings

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// let theContain = document.querySelector(".the-contain").style.display = "none";
-
-let thankYou = document.querySelector(".thankyou").style.display = "none";
+let thanYoun = document.querySelector(".thankyoun").style.display = "none";
 
 // get the DOM
 let ratingOne = document.querySelectorAll(".rating-1");
@@ -10,19 +8,22 @@ let rateTwo = document.getElementById("rate2");
 let rateThree = document.getElementById("rate3");
 let rateFour = document.getElementById("rate4");
 let span = document.getElementById('rating');
+let submit = document.getElementById("submit");
+let thankYoun = document.querySelector(".thankyoun");
+let theContain = document.querySelector(".the-contain")
 console.log( rate);
 
 //check rate when clicked
+
 for(let i = 0; i < ratingOne.length; i++){
-    // getting the clicked number value property from 1 to 5//
-    let rating = ratingOne[i].innerHTML;
-
-    // looping through the numbers to check for a previously selected rating and removing if such. //
-    // Necessary in order to have only one rating selected at a time //
-    for (let i = 0; i < ratingOne.length; i++) {
-              ratingOne[i].classList.contains('selected')
-              ratingOne[i].classList.remove('selected');
+  // Listening for click on each event
+  ratingOne[i].addEventListener('click', () =>{
+    // get the clicks from start to end
+    rating = ratingOne[i].innerHTML
+    //removing previously clicked rate if another is clicked.
+    for(let i = 0; i < ratingOne.length; i++){
+      ratingOne[i].classList.contains('seleted') 
+      ratingOne[i].classList.remove('selected')
+    }
+  })
 }
-}
-
-// add the selected class which highlights the selected rating //
